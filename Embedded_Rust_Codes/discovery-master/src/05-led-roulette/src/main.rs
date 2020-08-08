573b60935e50aa9e0bc02f2d252e596819c4035e
tanveerahmed98 <tanveer.ahm98@gmail.com>
2020-08-09 00:30:10 +0500

    Project Codes

--- Embedded_Rust_Codes/discovery-master/src/05-led-roulette/src/main.rs
+++ Embedded_Rust_Codes/discovery-master/src/05-led-roulette/src/main.rs
@@ -2,14 +2,22 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, prelude::*, Delay, Leds};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, Leds) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let ms = 50_u8;
+    loop {
+        for curr in 0..8 {
+            let next = (curr + 1) % 8;
+
+            leds[next].on();
+            delay.delay_ms(ms);
+            leds[curr].off();
+            delay.delay_ms(ms);
+        }
+    }
 }
+
